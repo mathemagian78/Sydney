9216b15fa3d72ed3b6856c638e4580c2ff1219b3
Mathemagician78 <60906110+Mathemagician78@users.noreply.github.com>
2020-05-31 00:29:43 -0700

    Updates

---
+++ dev/precache-manifest.896630fd9cfdb0c682ee03bf2e699894.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae686f6887010046d27f562ba3773e2",
+    "url": "/Sydney/devassets/Celery.png"
+  },
+  {
+    "revision": "2c564f11a5d4c315138ee0cb9474792a",
+    "url": "/Sydney/devassets/MyLogicPuzzle-Solved.png"
+  },
+  {
+    "revision": "1263cea2be926ca240630a28dc45fa19",
+    "url": "/Sydney/devassets/Poof.png"
+  },
+  {
+    "revision": "0fb6f93a0fc9e88b4e3ba420107c87ea",
+    "url": "/Sydney/devassets/Room.png"
+  },
+  {
+    "revision": "9ed6cb8feb446c130e2cb805065b5b7a",
+    "url": "/Sydney/devassets/Skull.png"
+  },
+  {
+    "revision": "9a2588a1a1a339966b3675117ecc0913",
+    "url": "/Sydney/devassets/Thunder.png"
+  },
+  {
+    "revision": "bd2bfd2402a6df2a9747ca86ba4b5a87",
+    "url": "/Sydney/devassets/Thunder2.png"
+  },
+  {
+    "revision": "90b7be82cd37eb98b01863f50b84ab62",
+    "url": "/Sydney/devassets/bunny-hop-spritesheet.png"
+  },
+  {
+    "revision": "68915bb265ad4941b284d5b28494dc3d",
+    "url": "/Sydney/devassets/carrot.png"
+  },
+  {
+    "revision": "37926d1723a23064bd4b09aeeed3daca",
+    "url": "/Sydney/devassets/carrotChase.wav"
+  },
+  {
+    "revision": "c9dd2677dfd4eba58e3ed0efd8ef4795",
+    "url": "/Sydney/devassets/carrotTime.png"
+  },
+  {
+    "revision": "004817e878fdab981a83b7e3c0ad0939",
+    "url": "/Sydney/devassets/citations.md"
+  },
+  {
+    "revision": "0ac26c5a70b28d696adba52a4074c533",
+    "url": "/Sydney/devassets/environment_forest_alt1.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/Sydney/devassets/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/Sydney/devassets/font.xml"
+  },
+  {
+    "revision": "8822c0c57b608c01898755ea857b4952",
+    "url": "/Sydney/devassets/fox-SWEN-bright.png"
+  },
+  {
+    "revision": "f38d7c246c961ee84fd2fc51147ac837",
+    "url": "/Sydney/devassets/frisbee.png"
+  },
+  {
+    "revision": "5c5eb626426eb420f3e5f4ca68c4cfb0",
+    "url": "/Sydney/devassets/grass.png"
+  },
+  {
+    "revision": "1091ad6d4686178a215475256cd015ec",
+    "url": "/Sydney/devassets/music.mp3"
+  },
+  {
+    "revision": "3cddec26f5730efd19bbea57323b5066",
+    "url": "/Sydney/devassets/oneBark.wav"
+  },
+  {
+    "revision": "8c17c1515edf3260716c7812c638e3cb",
+    "url": "/Sydney/devassets/pa2.png"
+  },
+  {
+    "revision": "59c0357f84d90e4bb344e5a8a591d917",
+    "url": "/Sydney/devassets/park.png"
+  },
+  {
+    "revision": "6814569ed4dcefaba2ed40a5e803782a",
+    "url": "/Sydney/devassets/parkButton.png"
+  },
+  {
+    "revision": "e0d390c783f610d638e5c34e8005772c",
+    "url": "/Sydney/devassets/pleasing-bell.wav"
+  },
+  {
+    "revision": "cae8bdd56df6425d46278ab33a748ee9",
+    "url": "/Sydney/devassets/pop.ogg"
+  },
+  {
+    "revision": "4784118f47d91136d4cba54fee347994",
+    "url": "/Sydney/devassets/rabbit.png"
+  },
+  {
+    "revision": "e7564671b32f498a76f09c4ff3814906",
+    "url": "/Sydney/devassets/roomButton.png"
+  },
+  {
+    "revision": "3a9c85fe5ad487a578aa67a6098fb6ac",
+    "url": "/Sydney/devassets/screenshot_1.png"
+  },
+  {
+    "revision": "bfba3c27e91bea02b43c2a73125e70d7",
+    "url": "/Sydney/devassets/snowButton.png"
+  },
+  {
+    "revision": "bf83067c40d73877c5bb5291e2ed31bf",
+    "url": "/Sydney/devassets/snowman.png"
+  },
+  {
+    "revision": "c4ac19c291e5b1e4bb46c9301953628f",
+    "url": "/Sydney/devassets/snowy.png"
+  },
+  {
+    "revision": "649356d9a68f2b89f6dee1a909fcdb9c",
+    "url": "/Sydney/devassets/snowyFont.fnt"
+  },
+  {
+    "revision": "60801a5d5eb247de3bff71bb754fbf5d",
+    "url": "/Sydney/devassets/snowyFont.png"
+  },
+  {
+    "revision": "078d739f9baf3d5cfbd92c9fcd661f59",
+    "url": "/Sydney/devassets/thunderbark.wav"
+  },
+  {
+    "revision": "1286ed7b814c70e7844f2ac01f1a3a78",
+    "url": "/Sydney/devassets/wave.wav"
+  },
+  {
+    "revision": "592f044bf13557d9f9aa6ec9e1beed83",
+    "url": "/Sydney/devassets/win.wav"
+  },
+  {
+    "revision": "a66507c80c238a10bf65",
+    "url": "/Sydney/devgen.main.9ecff4044fb8b466c295.bundle.js"
+  },
+  {
+    "revision": "b2512ad1dc1074ce2bf5",
+    "url": "/Sydney/devgen.vendors.ed464eb967a0780a06d8.bundle.js"
+  },
+  {
+    "revision": "a67b2ad284dda4d3579a288ef39af6fc",
+    "url": "/Sydney/devindex.html"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "/Sydney/devmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/Sydney/devsw.js"
+  }
+]);
---
+++ precache-manifest.b5c0d9bfb43426daa7d7fd063ca2136e.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae686f6887010046d27f562ba3773e2",
+    "url": "assets/Celery.png"
+  },
+  {
+    "revision": "2c564f11a5d4c315138ee0cb9474792a",
+    "url": "assets/MyLogicPuzzle-Solved.png"
+  },
+  {
+    "revision": "1263cea2be926ca240630a28dc45fa19",
+    "url": "assets/Poof.png"
+  },
+  {
+    "revision": "0fb6f93a0fc9e88b4e3ba420107c87ea",
+    "url": "assets/Room.png"
+  },
+  {
+    "revision": "9ed6cb8feb446c130e2cb805065b5b7a",
+    "url": "assets/Skull.png"
+  },
+  {
+    "revision": "9a2588a1a1a339966b3675117ecc0913",
+    "url": "assets/Thunder.png"
+  },
+  {
+    "revision": "bd2bfd2402a6df2a9747ca86ba4b5a87",
+    "url": "assets/Thunder2.png"
+  },
+  {
+    "revision": "90b7be82cd37eb98b01863f50b84ab62",
+    "url": "assets/bunny-hop-spritesheet.png"
+  },
+  {
+    "revision": "68915bb265ad4941b284d5b28494dc3d",
+    "url": "assets/carrot.png"
+  },
+  {
+    "revision": "37926d1723a23064bd4b09aeeed3daca",
+    "url": "assets/carrotChase.wav"
+  },
+  {
+    "revision": "c9dd2677dfd4eba58e3ed0efd8ef4795",
+    "url": "assets/carrotTime.png"
+  },
+  {
+    "revision": "004817e878fdab981a83b7e3c0ad0939",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "0ac26c5a70b28d696adba52a4074c533",
+    "url": "assets/environment_forest_alt1.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "assets/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "assets/font.xml"
+  },
+  {
+    "revision": "8822c0c57b608c01898755ea857b4952",
+    "url": "assets/fox-SWEN-bright.png"
+  },
+  {
+    "revision": "f38d7c246c961ee84fd2fc51147ac837",
+    "url": "assets/frisbee.png"
+  },
+  {
+    "revision": "5c5eb626426eb420f3e5f4ca68c4cfb0",
+    "url": "assets/grass.png"
+  },
+  {
+    "revision": "1091ad6d4686178a215475256cd015ec",
+    "url": "assets/music.mp3"
+  },
+  {
+    "revision": "3cddec26f5730efd19bbea57323b5066",
+    "url": "assets/oneBark.wav"
+  },
+  {
+    "revision": "8c17c1515edf3260716c7812c638e3cb",
+    "url": "assets/pa2.png"
+  },
+  {
+    "revision": "59c0357f84d90e4bb344e5a8a591d917",
+    "url": "assets/park.png"
+  },
+  {
+    "revision": "6814569ed4dcefaba2ed40a5e803782a",
+    "url": "assets/parkButton.png"
+  },
+  {
+    "revision": "e0d390c783f610d638e5c34e8005772c",
+    "url": "assets/pleasing-bell.wav"
+  },
+  {
+    "revision": "cae8bdd56df6425d46278ab33a748ee9",
+    "url": "assets/pop.ogg"
+  },
+  {
+    "revision": "4784118f47d91136d4cba54fee347994",
+    "url": "assets/rabbit.png"
+  },
+  {
+    "revision": "e7564671b32f498a76f09c4ff3814906",
+    "url": "assets/roomButton.png"
+  },
+  {
+    "revision": "3a9c85fe5ad487a578aa67a6098fb6ac",
+    "url": "assets/screenshot_1.png"
+  },
+  {
+    "revision": "bfba3c27e91bea02b43c2a73125e70d7",
+    "url": "assets/snowButton.png"
+  },
+  {
+    "revision": "bf83067c40d73877c5bb5291e2ed31bf",
+    "url": "assets/snowman.png"
+  },
+  {
+    "revision": "c4ac19c291e5b1e4bb46c9301953628f",
+    "url": "assets/snowy.png"
+  },
+  {
+    "revision": "649356d9a68f2b89f6dee1a909fcdb9c",
+    "url": "assets/snowyFont.fnt"
+  },
+  {
+    "revision": "60801a5d5eb247de3bff71bb754fbf5d",
+    "url": "assets/snowyFont.png"
+  },
+  {
+    "revision": "078d739f9baf3d5cfbd92c9fcd661f59",
+    "url": "assets/thunderbark.wav"
+  },
+  {
+    "revision": "1286ed7b814c70e7844f2ac01f1a3a78",
+    "url": "assets/wave.wav"
+  },
+  {
+    "revision": "592f044bf13557d9f9aa6ec9e1beed83",
+    "url": "assets/win.wav"
+  },
+  {
+    "revision": "3db9c2676660a1ab35aa1698d452e578",
+    "url": "index.html"
+  },
+  {
+    "revision": "37572a4822cb45cf499a",
+    "url": "main.fc9b0549223d785d3c91.bundle.js"
+  },
+  {
+    "revision": "fc8cefcfb102b35601f09d51ee2512ef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "b2512ad1dc1074ce2bf5",
+    "url": "vendors.ed464eb967a0780a06d8.bundle.js"
+  }
+]);
